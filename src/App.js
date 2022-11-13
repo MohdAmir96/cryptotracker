fd6deaf14f66febcdbd19eb12d707adbbfd2ed3a
Mohd <amirmanawwar@gmail.com>
2022-11-13 11:00:28 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import DashboardPage from "./pages/dashboard";
+
 function App() {
-  return <div>test</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
